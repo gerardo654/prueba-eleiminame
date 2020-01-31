df74623ca520f512c15c1d8f442c133222b803b0
Gustavo Peralta <gperalta@grupobecm.com>
2020-01-31 12:54:53 -0600

    mamamama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PruebaComponent } from './prueba/prueba.component';
+import { OtroComponent } from './otro/otro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'prueba', component: PruebaComponent},
+  {path: 'otro', component: OtroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './prueba/prueba.component';
+import { OtroComponent } from './otro/otro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    OtroComponent
   ],
   imports: [
     BrowserModule,
